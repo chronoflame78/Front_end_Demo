774fb96dbc4e1f06b645079ecdd45b38a118c794
chronoflame78 <gaovanthet555@gmail.com>
2023-10-03 18:13:03 +0700

    update algo file

--- algorithm.js
+++ algorithm.js
@@ -9,6 +9,20 @@ function greatestCommonPrimeDivisor(a, b){
 
 }
 
+const findGreatestCommonDivisor = (a, b) => {
+    let gcd = -1;
+    for(let i = 2; i <= a; i ++){
+        if(a % i === 0 && b % i === 0){
+            gcd = i;
+        }
+    }
+    return gcd;
+}
+
+function lcm(a, b) {
+    return a * (b / greatestCommonPrimeDivisor(a, b));
+}
+
 function isPrime(n){
     if(n === 1) return true;
     if(n === 0) return false;
@@ -256,3 +270,240 @@ function maxFraction(numerators, denominators){
     
     }
 }
+
+
+// In JavaScript, both Map and Set are built-in objects that allow you to work with collections of values. They have distinct use cases and characteristics:
+
+// Map:
+// Key-Value Pairs: A Map is a collection of key-value pairs where each key can be of any data type (including objects, functions, and primitive values) and each value can also be of any data type.
+
+// Order of Elements: Unlike plain JavaScript objects, Map maintains the order of elements based on the order of insertion. This makes it useful for scenarios where the order of elements matters.
+
+// Duplicate Keys: A Map allows keys to be duplicated, but each key maps to a unique value. This means that you can't have two identical keys with different values.
+
+// Iterating Elements: You can easily iterate over the elements of a Map using methods like forEach, for...of, or by converting it to an array with the Array.from method.
+
+// Example of creating and using a Map:
+
+
+const myMap = new Map();
+
+myMap.set('name', 'John');
+myMap.set('age', 30);
+
+console.log(myMap.get('name')); // Output: 'John'
+console.log(myMap.has('age'));   // Output: true
+
+myMap.forEach((value, key) => {
+    console.log(`${key}: ${value}`);
+});
+
+// Set:
+// Collection of Unique Values: A Set is a collection of unique values. It can contain values of any data type and automatically removes duplicate values.
+
+// Order of Elements: Similar to Map, Set also maintains the order of elements based on insertion order.
+
+// No Key-Value Pairs: Unlike Map, Set doesn't store key-value pairs; it only stores unique values.
+
+// Iterating Elements: You can iterate over the elements of a Set using methods like forEach, for...of, or by converting it to an array with the Array.from method.
+
+// Example of creating and using a Set:
+
+const mySet = new Set();
+
+mySet.add(1);
+mySet.add(2);
+mySet.add(2); // Duplicate values are ignored
+
+console.log(mySet.has(2)); // Output: true
+
+mySet.forEach(value => {
+    console.log(value);
+});
+// In summary, Map is used when you need to associate values with unique keys, while Set is used when you need to work with a collection of unique values without associated keys. Both are powerful tools for working with data in JavaScript.
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    for (let j = s.length - 1; j >= 0; j--) {
+        let i = 0,
+        k = j;
+        while (k < s.length) {
+            let substr = s.substring(i, k + 1);
+            if (isPalindrome(substr)) return substr;
+            i++, k++;
+        }
+    }
+    return "";
+
+    //time O(n^3)
+};
+
+let isPalindrome = (str) => {
+    if(!str || !str.length) return false;
+    if(str.length === 1) return true;
+    for(let i = 0; i < Math.floor(str.length/2); i++){
+        if(str[i] !== str[str.length - 1 - i])
+        return false;
+    }
+    return true;
+}
+
+
+//7. Reverse Integer
+
+var reverse = function(x) {
+    if(x > (Math.pow(2, 31) - 1)) return 0;
+    if(x < Math.pow(-2, 31)) return 0;
+    const str = `${x}`;
+    let subStr = '';
+    if(str[0] === '-'){
+        subStr = str.slice(1, str.length);
+        subStr = subStr.split("").reverse().join("");
+        if((parseInt(subStr) * -1) > (Math.pow(2, 31) - 1)) return 0;
+        if((parseInt(subStr) * -1) < Math.pow(-2, 31)) return 0;
+        return parseInt(subStr) * -1;
+    }
+    else{
+        let result = parseInt(str.split("").reverse().join(""));
+        if(result > (Math.pow(2, 31) - 1)) return 0;
+        if(result < Math.pow(-2, 31)) return 0;
+        return result;
+    }
+};
+
+//605. Can Place Flowers
+
+var canPlaceFlowers = function(flowerbed, n) {
+    let count = 0;
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] === 0) {
+            // Check if the current plot and its adjacent plots are empty.
+            const prevEmpty = i === 0 || flowerbed[i - 1] === 0;
+            const nextEmpty = i === flowerbed.length - 1 || flowerbed[i + 1] === 0;
+
+            if (prevEmpty && nextEmpty) {
+                // Plant a flower at the current plot.
+                flowerbed[i] = 1;
+                count++;
+
+                if (count >= n) {
+                    return true; // We've planted enough flowers.
+                }
+            }
+        }
+    }
+
+    return count >= n; // Check if we've planted enough flowers.
+
+    
+// The time complexity of the canPlaceFlowers function is O(N), where N is the length of the flowerbed array. This is because the function iterates through the flowerbed array once, performing constant-time operations at each step.
+
+// The space complexity of the function is O(1), which means it uses a constant amount of extra space regardless of the size of the input flowerbed array. The additional space used by the function is for variables like count, prevEmpty, nextEmpty, and the loop counter i, all of which occupy a constant amount of memory. The function does not create any data structures or allocate memory that scales with the size of the input array, making its space complexity constant.
+};
+
+//345. Reverse Vowels of a String
+var reverseVowels = function(s) {
+    // Helper function to check if a character is a vowel
+    const isVowel = char => 'aeiouAEIOU'.includes(char);
+
+    // Convert the string into an array of characters
+    const charArray = s.split('');
+
+    // Collect the indexes and values of vowels
+    const vowelIndexes = [];
+    const vowelValues = [];
+
+    for (let i = 0; i < charArray.length; i++) {
+        if (isVowel(charArray[i])) {
+            vowelIndexes.push(i);
+            vowelValues.push(charArray[i]);
+        }
+    }
+
+    // Reverse the array of vowel values
+    vowelValues.reverse();
+
+    // Set the reversed values back using the collected indexes
+    for (let i = 0; i < vowelIndexes.length; i++) {
+        charArray[vowelIndexes[i]] = vowelValues[i];
+    }
+
+    // Join the array back into a string
+    return charArray.join('');
+
+}
+
+//394. Decode String
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function(s) {
+    let stack = [];
+    let curNum = 0;
+    let curStr = "";
+    
+    for(let char of s) {
+        if(char >= '0' && char <= '9') {
+            // Calculate the complete number.
+            curNum = curNum * 10 + parseInt(char);
+        } else if(char === '[') {
+            // Push the current number and string to the stack.
+            stack.push(curStr);
+            stack.push(curNum);
+            // Reset the current number and string.
+            curNum = 0;
+            curStr = "";
+        } else if(char === ']') {
+            // Pop the last number and string from the stack.
+            let prevNum = stack.pop();
+            let prevStr = stack.pop();
+            // Append the current string repeated prevNum times to the prevStr.
+            curStr = prevStr + curStr.repeat(prevNum);
+        } else {
+            // Append the character to the current string.
+            curStr += char;
+        }
+    }
+    
+    return curStr;
+
+};
+
+
+
+//Graphs - DFS  (Depth First Search)
+
+//841. Keys and Rooms
+/**
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+var canVisitAllRooms = function(rooms) {
+    const visited = new Set(); // To keep track of visited rooms.
+  
+    const dfs = (room) => {
+        if (visited.has(room)) return; // Skip if the room is already visited.
+        
+        visited.add(room); // Mark the room as visited.
+        
+        // Visit all the rooms that can be unlocked from the current room.
+        for (const key of rooms[room]) {
+        dfs(key);
+        }
+    };
+    
+    // Start DFS from room 0 (the initial room).
+    dfs(0);
+    
+    // Check if all rooms are visited.
+    return visited.size === rooms.length;
+};
+
+// Time Complexity: The time complexity of the DFS algorithm is O(V + E), where V is the number of vertices (rooms) and E is the number of edges (keys that unlock rooms). In this context, V represents the number of rooms, and E represents the total number of keys in all rooms.
+
+// Space Complexity: The space complexity is O(V), where V is the number of rooms. This space is used to store the visited set, which keeps track of the visited rooms.
