e3494a9f6b6e9b55800c85bf2de28d0a9f86dd66
chronoflame78 <gaovanthet555@gmail.com>
2023-10-09 22:44:08 +0700

    updates

--- algorithm.js
+++ algorithm.js
@@ -48,77 +48,130 @@ function maxFraction(numerators, denominators){
         }
     }
     return pos;
+}
 
-    function powerOfThreeNotNine(m, n){
-        var count = 0;
-        for(var i = m; i <= n; i++){
-            if(isPowerOfThree(i) === true && isPowerOfNine(i) === false){
-                count++;
-            }
+function powerOfThreeNotNine(m, n){
+    var count = 0;
+    for(var i = m; i <= n; i++){
+        if(isPowerOfThree(i) === true && isPowerOfNine(i) === false){
+            count++;
         }
-        
-        return count;
     }
     
-    function isPowerOfThree(x){
-        while (x % 3 == 0) {
-            x /= 3;
-        }
-        return x == 1;
-    }
-    
-    function isPowerOfNine(y){
-        while (y % 9 == 0) {
-            y /= 9;
-        }
-        return y == 1;
-    }
+    return count;
+}
 
-    function lineUp(s, t){
-        var arr = s.split('');
-        for(var i = 0; i < t; i++){
-            for(var j = 0; j < arr.length; j++){
-                if(arr[j] == 'B' && arr[j+1]=='G'){
-                    arr[j] = 'G';
-                    arr[j+1] = 'B';
-                    j += 1;
-                }
+function isPowerOfThree(x){
+    while (x % 3 == 0) {
+        x /= 3;
+    }
+    return x == 1;
+}
+
+function isPowerOfNine(y){
+    while (y % 9 == 0) {
+        y /= 9;
+    }
+    return y == 1;
+}
+
+function lineUp(s, t){
+    var arr = s.split('');
+    for(var i = 0; i < t; i++){
+        for(var j = 0; j < arr.length; j++){
+            if(arr[j] == 'B' && arr[j+1]=='G'){
+                arr[j] = 'G';
+                arr[j+1] = 'B';
+                j += 1;
             }
         }
-        return arr.join('');
     }
+    return arr.join('');
+}
 
-    //check if nam nhuan
-    function run(year){
-        if ((year%4==0&&year%100!=0&&year%400!=0)||(year%100==0&&year%400==0)){
-        console.log(year + " is a leap year");
-        }else{
-        console.log(year + " is not a leap year");
+//check if nam nhuan
+function run(year){
+    if ((year%4==0&&year%100!=0&&year%400!=0)||(year%100==0&&year%400==0)){
+    console.log(year + " is a leap year");
+    }else{
+    console.log(year + " is not a leap year");
+    }
+}
+
+//print out even numbers from 1 to n
+function run(n){
+    var output = "";
+    var arr = [];
+    var i = 1;
+    while(i <= n) {
+        if(i%2 == 0)
+        {
+        arr.push(i);
+        output = arr.join(',');		
+        }
+        i++;
+    }
+    
+    console.log(output);
+}
+
+//count occurence in arr
+function countOccurrences(arr) {
+    // viết code ở đây.
+    return arr.reduce(function(x1,x2){
+      var check = x1[x2] + 1; //obj['a'] = 1;
+      if(check){
+        x1[x2] = check;
+      }else{
+        x1[x2] = 1;
+      }
+      return x1;
+    }, {});
+}
+
+const countOccurrences2 = (arr) => {
+    let hash = {};
+    for(let i = 0; i < arr.length; i++){
+        if(!hash[arr[i]]){
+            hash[arr[i]] = 1;
+        }
+        else{
+            hash[arr[i]] += 1;
         }
     }
-
-    //print out even numbers from 1 to n
-    function run(n){
-        var output = "";
-        var arr = [];
-        var i = 1;
-        while(i <= n) {
-            if(i%2 == 0)
-            {
+    return hash;
+}  
+  
+//find a number satisfied frequency k
+function kFrequency(a, k){
+    var index = 1000;
+    var result = -1;
+    var obj1 = countOccurrences(a);
+    var arr = [];
+    console.log(obj1);
+    for(var i in obj1){
+        console.log(obj1[i]);
+        if(obj1[i]==k){
             arr.push(i);
-            output = arr.join(',');		
-            }
-            i++;
         }
-        
-        console.log(output);
     }
+    console.log(arr);
+    if(arr.length > 0){
+        for(var num of arr)
+        {
+            if(a.indexOf(num) < index){
+                index = a.indexOf(num);          
+            }
+        
+        }
+        result = a[index];
+    } 
+    return result;
+}
 
-    //count occurence in arr
-    function countOccurrences(arr) {
-        // viết code ở đây.
+function countOccurrences(arr) {
         return arr.reduce(function(x1,x2){
-          var check = x1[x2] + 1; //obj['a'] = 1;
+          var check = x1[x2] + 1;
           if(check){
             x1[x2] = check;
           }else{
@@ -126,149 +179,109 @@ function maxFraction(numerators, denominators){
           }
           return x1;
         }, {});
-      }
-      
-    //find a number satisfied frequency k
-    function kFrequency(a, k){
-        var index = 1000;
-        var result = -1;
-        var obj1 = countOccurrences(a);
-        var arr = [];
-        console.log(obj1);
-        for(var i in obj1){
-            console.log(obj1[i]);
-            if(obj1[i]==k){
-                arr.push(i);
-            }
-        }
-        console.log(arr);
-        if(arr.length > 0){
-            for(var num of arr)
-            {
-                if(a.indexOf(num) < index){
-                    index = a.indexOf(num);          
-                }
-            
-            }
-            result = a[index];
-        } 
-        return result;
+}
+
+console.log(kFrequency([1,1,1,3,2,3,2,4,4,4,1], 1));
+//find a number satisfied frequency k _ END
+
+//reverse string
+function run(str_input ) {
+    var str = str_input.split("").reverse().join("");
+    return str;
+}
+
+//replace characters in string
+function run(str) {
+    for(i=0;i<str.length;i++){
+    str = str.replace("a","4");
+    str = str.replace("e","3");
+    str = str.replace("i","1");
+    str = str.replace("o","0");
+    str = str.replace("s","5");
     }
+    str = str.trim();
+    console.log(str);
     
-    function countOccurrences(arr) {
-            return arr.reduce(function(x1,x2){
-              var check = x1[x2] + 1;
-              if(check){
-                x1[x2] = check;
-              }else{
-                x1[x2] = 1;
-              }
-              return x1;
-            }, {});
+}
+
+//find max, min 
+function run(a, b, c, d) {  //when has 4 input
+    var arr = Object.values(arguments);
+      var max = arr.reduce((x,y) => Math.max(x,y));
+      var min = arr.reduce((x,y) => Math.min(x,y));
+      console.log(max+" "+min);
+  }
+
+  function run(...rest) { //when unknown number of input
+    var arr = Object.values(arguments);
+      var max = arr.reduce((x,y) => Math.max(x,y));
+      var min = arr.reduce((x,y) => Math.min(x,y));
+      console.log(max+" "+min);
+  }
+
+  //doi 2 so cuoi 1 so  ex:12345 -> 12354
+//   public static void main(String[] args) {
+//     Scanner scanner = new Scanner(System.in);
+//     int n = scanner.nextInt();
+
+//     n = (n/100)*100 + (n % 10)*10 + ((n % 100)/10);
+
+//     System.out.println(n);
+// }
+
+
+  //switch case example
+// switch(num)
+//     {
+//         case "0":
+//         case "1":
+//         case "2":
+//         case "3":
+//         case "4":
+//         case "5":
+//         case "6":
+//         case "7":
+//         case "8":
+//         case "9":
+//             System.out.println("You pressed "+ num);
+//             break;
+//         default:
+//             System.out.println("Not allowed!");
+//             break;
+//switch case example_END
+
+  //tim trang arr tich cua 2 so lon nhat  vd: [13,4,5,-7,2,-6,-5] kq:65
+function maxMultiplication(a){
+    a.sort((x,y) => x-y);
+    var max = a[0] * a[1];
+    if(max < a[a.length-1] * a[a.length-2]){
+        max = a[a.length-1] * a[a.length-2];
     }
-    
-    console.log(kFrequency([1,1,1,3,2,3,2,4,4,4,1], 1));
-    //find a number satisfied frequency k _ END
+    return max;
+}
 
-    //reverse string
-    function run(str_input ) {
-        var str = str_input.split("").reverse().join("");
-        return str;
-    }
-
-    //replace characters in string
-    function run(str) {
-        for(i=0;i<str.length;i++){
-        str = str.replace("a","4");
-        str = str.replace("e","3");
-        str = str.replace("i","1");
-        str = str.replace("o","0");
-        str = str.replace("s","5");
-        }
-        str = str.trim();
-        console.log(str);
-        
-    }
-
-    //find max, min 
-    function run(a, b, c, d) {  //when has 4 input
-        var arr = Object.values(arguments);
-          var max = arr.reduce((x,y) => Math.max(x,y));
-          var min = arr.reduce((x,y) => Math.min(x,y));
-          console.log(max+" "+min);
-      }
-    
-      function run(...rest) { //when unknown number of input
-        var arr = Object.values(arguments);
-          var max = arr.reduce((x,y) => Math.max(x,y));
-          var min = arr.reduce((x,y) => Math.min(x,y));
-          console.log(max+" "+min);
-      }
-
-      //doi 2 so cuoi 1 so  ex:12345 -> 12354
-    //   public static void main(String[] args) {
-    //     Scanner scanner = new Scanner(System.in);
-    //     int n = scanner.nextInt();
-
-    //     n = (n/100)*100 + (n % 10)*10 + ((n % 100)/10);
-
-    //     System.out.println(n);
-    // }
-
-
-      //switch case example
-    // switch(num)
-    //     {
-    //         case "0":
-    //         case "1":
-    //         case "2":
-    //         case "3":
-    //         case "4":
-    //         case "5":
-    //         case "6":
-    //         case "7":
-    //         case "8":
-    //         case "9":
-    //             System.out.println("You pressed "+ num);
-    //             break;
-    //         default:
-    //             System.out.println("Not allowed!");
-    //             break;
-    //switch case example_END
-    
-      //tim trang arr tich cua 2 so lon nhat  vd: [13,4,5,-7,2,-6,-5] kq:65
-    function maxMultiplication(a){
-        a.sort((x,y) => x-y);
-        var max = a[0] * a[1];
-        if(max < a[a.length-1] * a[a.length-2]){
-            max = a[a.length-1] * a[a.length-2];
-        }
-        return max;
-    }
-    
-    //DAN có một người bạn rất thích chơi lego nên muốn ra một câu đố. DAN đang có vô số mảnh lego với chiều rộng là 1
-    // và chiều dài là a và b. Hãy giúp người bạn của DAN tìm số mảnh lego ít nhất  mà có thể lắp được một hàng lego với 
-    //chiều dài là n và chiều rộng là 1, nếu không có cách nào hãy trả về -1.
-    //giai pt ax+by = n
-    function lego(a,b,n){
-        let arr = [];
-        for (let i = 0; i * a <= n; i++) 
+//DAN có một người bạn rất thích chơi lego nên muốn ra một câu đố. DAN đang có vô số mảnh lego với chiều rộng là 1
+// và chiều dài là a và b. Hãy giúp người bạn của DAN tìm số mảnh lego ít nhất  mà có thể lắp được một hàng lego với 
+//chiều dài là n và chiều rộng là 1, nếu không có cách nào hãy trả về -1.
+//giai pt ax+by = n
+function lego(a,b,n){
+    let arr = [];
+    for (let i = 0; i * a <= n; i++) 
+        { 
+            // check if it is satisfying the equation 
+            if ((n - (i * a)) % b === 0) 
             { 
-                // check if it is satisfying the equation 
-                if ((n - (i * a)) % b === 0) 
-                { 
-                    arr.push(i +  (n - (i * a)) / b); 
-                } 
+                arr.push(i +  (n - (i * a)) / b); 
             } 
-        if(arr.length === 0)
-        return -1;
-    
-        arr.sort((a,b) => {
-            return a-b;
-        });
-        return arr[0];
-    
-    }
+        } 
+    if(arr.length === 0)
+    return -1;
+
+    arr.sort((a,b) => {
+        return a-b;
+    });
+    return arr[0];
+
 }
 
 
